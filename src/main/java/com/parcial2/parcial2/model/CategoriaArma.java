54b63cffdce1345928dab38279de9468bbf5acf0
Cristian <cr.ferreiraa@duocuc.cl>
2026-05-06 00:20:02 -0400

    Modelo arma

--- src/main/java/com/parcial2/parcial2/model/CategoriaArma.java
+++ src/main/java/com/parcial2/parcial2/model/CategoriaArma.java
@@ -1,9 +1,14 @@
 package com.parcial2.parcial2.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,12 +20,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table (name = "categoria_arma")
 public class CategoriaArma {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     @NotBlank(message = "El nombre de la categoria no puede estar vacio!!")
+    @Column(nullable = false, length =  100)
     private String nombre;
 
+    @OneToMany(mappedBy = "categoriaArma")
+    private List<TipoArma> tipoArmas;
+
+    
+
+    
 }
